18412bb2e9a9070070f6ba9d52cc2d0c5b0815f5
shiva119127 <shiva.119127@gmail.com>
2026-04-09 23:44:40 +0530

    new commit

---
+++ Sum of Natural Numbers.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int findSum(int n) {
+        int summ=0;
+        for(int i=0;i<=n;i++){
+            summ+=i;
+            
+        }
+        return summ;
+        
+    }
+};
